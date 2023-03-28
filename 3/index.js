6a0427d143cea8825bff232497756fabe71c129c
Pavlo4620 <striyvus800@gmail.com>
2023-03-28 20:05:33 +0300

    first commit

---
+++ 3/index.js
@@ -0,0 +1,4 @@
+var a = 5, b = 2;
+console.log(a, b);
+a = b;
+console.log(a, b);
---
+++ 6/index.js
@@ -0,0 +1,15 @@
+let myName = "Pavlo";
+let lastName = "Striyvus";
+let myGrup = "kn-321";
+let year = "2005";
+let married = false;
+
+console.log(year);
+console.log(married);
+console.log(myName + " " + lastName + " " + myGrup);
+
+let myNull = null;
+let myUndefined = undefined;
+
+console.log(typeof myNull);
+console.log(typeof myUndefined);
